587a4288a783dd5830fe25ba36e9fccfe92a5a99
jopajak <jopajak@student.agh.edu.pl>
2022-05-06 18:50:08 +0200

    Initial commit

---
+++ src/main/java/com/example/judocompetitionmanager/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.judocompetitionmanager;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
