e3e7d91e9c5cba34ad5ac42178026147e476d228
jopajak <jopajak@student.agh.edu.pl>
2022-05-22 16:28:40 +0200

    class Contestant and SET

--- src/main/java/com/example/judocompetitionmanager/ContestantsController.java
+++ src/main/java/com/example/judocompetitionmanager/ContestantsController.java
@@ -1,4 +1,20 @@
 package com.example.judocompetitionmanager;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+
 public class ContestantsController {
+
+    @FXML
+    TextArea contestantsScene;
+    public void print(ActionEvent e){
+        //contestantsScene.setText("seferg");
+        //System.out.println(contestantsScene.getText());
+    }
+    @FXML
+    public void onAreaClick(ActionEvent e){
+
+    }
+
 }
--- src/main/java/com/example/judocompetitionmanager/MainController.java
+++ src/main/java/com/example/judocompetitionmanager/MainController.java
@@ -2,10 +2,21 @@ package com.example.judocompetitionmanager;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.event.ActionEvent;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainController {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
 
     @FXML
     Label nameLabel;
@@ -35,12 +46,26 @@ public class MainController {
 
     @FXML
     public void onContestsClick(ActionEvent e) {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("myContestants-view.fxml"));
-
 
     }
     @FXML
     public void onMyContestantsClick(ActionEvent e) {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("contestants-view.fxml"));
+        try {
+            root = loader.load();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
 
+        ContestantsController contestantController = loader.getController();
+
+        stage = (Stage) ((Node)e.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        if (getClass().getResource("app.css") != null) {
+            scene.getStylesheets().add(getClass().getResource("def.css").toExternalForm());
+        }
+        stage.getIcons().add(new Image("icon.jpg"));
+        stage.setScene((Scene) scene);
+        stage.show();
     }
 }
