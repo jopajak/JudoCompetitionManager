0a81fc0fbbacad02a046a7bce2a8a17638105d35
nie_Igi_konto <jakub,dobosz01@gmail.com>
2022-05-26 10:24:13 +0200

    removed database folder

--- src/main/java/com/example/judocompetitionmanager/Testing.java
+++ src/main/java/com/example/judocompetitionmanager/Testing.java
@@ -9,27 +9,7 @@ import java.util.List;
 
 public class Testing {
     public static void main(String[] args) throws JSONException {
-        Database db = Database.getInstance();
-        db.writeString("user1", "Jakub Dobosz");
-        db.writeString("user2", "Marian Kowal");
 
-        System.out.println(db.readString("user2"));
-
-        List<String> list=new ArrayList<String>();
-        list.add("AGH Krakow");
-        list.add("12");
-        list.add("hard");
-        list.add("AGH Krakow");
-        list.add("12");
-        list.add("hard");
-
-        JSONObject jo = db.jsonify_competition(list);
-        db.writeJsonObject("Zawody1", jo);
-        JSONObject jo2 = db.readJsonObject("Zawody1");
-        String name = jo2.getString("organizer");
-        System.out.println(name);
-
-        Database.close();
 
     }
 }
