790b9b3ca608288bba5f90112a432199eafa6803
jopajak <jopajak@student.agh.edu.pl>
2022-05-26 09:56:41 +0200

    goBack button

--- src/main/java/com/example/judocompetitionmanager/MainScene.java
+++ src/main/java/com/example/judocompetitionmanager/MainScene.java
@@ -52,45 +52,45 @@ public class MainScene extends Application {
         //Group root = new Group();
         /**Scene scene = new Scene(root, 600, 400, Color.PURPLE);
 
-        stage.getIcons().add(new Image("icon.jpg"));
-        stage.setTitle("Judo Competition Manger");
-        //stage.setWidth(420);
-        //stage.setHeight(420);
-        //stage.setX(100);
-        //stage.setY(100);
-        //stage.setFullScreen(true);
-        stage.setFullScreenExitHint("to escape press Escape button ");
-        stage.setFullScreenExitKeyCombination(KeyCombination.valueOf("Esc"));
+         stage.getIcons().add(new Image("icon.jpg"));
+         stage.setTitle("Judo Competition Manger");
+         //stage.setWidth(420);
+         //stage.setHeight(420);
+         //stage.setX(100);
+         //stage.setY(100);
+         //stage.setFullScreen(true);
+         stage.setFullScreenExitHint("to escape press Escape button ");
+         stage.setFullScreenExitKeyCombination(KeyCombination.valueOf("Esc"));
 
-        Text text = new Text();
-        text.setText("Yeah.");
-        text.setX(50);
-        text.setY(50);
-        text.setFont(Font.font("Verdana", 50));
-        text.setFill(Color.LIMEGREEN);
+         Text text = new Text();
+         text.setText("Yeah.");
+         text.setX(50);
+         text.setY(50);
+         text.setFont(Font.font("Verdana", 50));
+         text.setFill(Color.LIMEGREEN);
 
-        Line line = new Line();
-        line.setStartX(200);
-        line.setStartY(200);
-        line.setEndX(500);
-        line.setEndY(200);
-        line.setStrokeWidth(5);
-        line.setStroke(Color.LIME);
-        line.setOpacity(0.5);
-        line.setRotate(45);
+         Line line = new Line();
+         line.setStartX(200);
+         line.setStartY(200);
+         line.setEndX(500);
+         line.setEndY(200);
+         line.setStrokeWidth(5);
+         line.setStroke(Color.LIME);
+         line.setOpacity(0.5);
+         line.setRotate(45);
 
-        Image image = new Image("icon.jpg");
-        ImageView iconView = new ImageView(image);
-        iconView.setX(400);
-        iconView.setX(350);
+         Image image = new Image("icon.jpg");
+         ImageView iconView = new ImageView(image);
+         iconView.setX(400);
+         iconView.setX(350);
 
 
-        //root.getChildren().add(text);
-        //root.getChildren().add(line);
-        //root.getChildren().add(iconView);
+         //root.getChildren().add(text);
+         //root.getChildren().add(line);
+         //root.getChildren().add(iconView);
 
-        stage.setScene(scene);
-        stage.show();
+         stage.setScene(scene);
+         stage.show();
          **/
     }
 
--- src/main/java/com/example/judocompetitionmanager/RankingController.java
+++ src/main/java/com/example/judocompetitionmanager/RankingController.java
@@ -3,13 +3,24 @@ package com.example.judocompetitionmanager;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class RankingController implements Initializable {
+
+    private Stage stage;
+    private Parent root;
+    private Scene scene;
 
     String[] contestants = {
             "Me",
@@ -48,5 +59,22 @@ public class RankingController implements Initializable {
 
     public void showProfile(){
         ;
+    }
+
+    public void goBack(MouseEvent mouseEvent) {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("main-view.fxml"));
+        try {
+            root = loader.load();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        stage = (Stage) ((Node)mouseEvent.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        if (getClass().getResource("app.css") != null) {
+            scene.getStylesheets().add(getClass().getResource("app.css").toExternalForm());
+        }
+        //stage.getIcons().add(new Image("icon.jpg"));
+        stage.setScene((Scene) scene);
+        stage.show();
     }
 }
