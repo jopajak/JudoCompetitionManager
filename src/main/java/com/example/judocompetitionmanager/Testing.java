85f1e908ebc5398a2df5bd7d437d0bedcdf49b0e
nie_Igi_konto <jakub,dobosz01@gmail.com>
2022-05-30 14:51:49 +0200

    addContestant restored

--- src/main/java/com/example/judocompetitionmanager/Testing.java
+++ src/main/java/com/example/judocompetitionmanager/Testing.java
@@ -18,9 +18,10 @@ public class Testing {
 
         List<String> list = Arrays.asList("Kuba", "Woje", "12", "78", "true", "M");
         List<String> list2 = Arrays.asList("Kuba1", "Woje2", "123", "78", "true", "M");
+        List<String> list3 = Arrays.asList("Kub", "Woje2", "123", "78", "true", "M");
 
         JSONObject jo = db.jsonify_contestant(list);
-        JSONObject jox = db.jsonify_contestant(list);
+        JSONObject jox = db.jsonify_contestant(list2);
 
         competitors.put("Competitor1", jo);
         competitors.put("CompetitorKuba", jox);
@@ -29,11 +30,8 @@ public class Testing {
 
         JSONObject jo2 = db.readJsonObject("competitors");
         System.out.println(jo2);
-        List list23 = db.getCompetitorsList();
-
-        System.out.println(list23);
-
-
+        List competitorsList = db.getCompetitorsList();
+        System.out.println(competitorsList);
 
 
         Database.close();
